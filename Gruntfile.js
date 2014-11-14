1fb9ae9bf88a1f67a0d2efcd394bbac90c480019
mryellow <mr-yellow@mr-yellow.com>
2014-11-15 06:22:36 +1100

    Initial files

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+'use strict';
+
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+
+	concat: {
+		options: {
+			stripBanners: {
+				separator: "\n",
+				block: true, /*! ...will be untouched... */
+				line: true
+			}
+		},
+		build: {
+			files: [
+				{'build/<%= pkg.name %>.css': ['src/**/*.css']},
+				{'build/<%= pkg.name %>.js': ['src/intro.txt', 'src/knockoutjs-sparkline.js', 'src/outro.txt']}
+			]
+		}
+	},
+
+	uglify: {
+		options: {
+			banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+			preserveComments: false,
+			compress: {
+				drop_console: true,
+			}
+		},
+		build: {
+			files: [
+				{
+					expand: true,
+					cwd: 'build/',
+					src: ['**/*.js','!**/*.min.js'],
+					dest: 'build/',
+					ext: '.min.js',
+					extDot: 'last',
+					filter: 'isFile'
+				}
+			]
+		}
+	},
+
+	copy: {
+		dist: {
+			files: [
+				{'dist/<%= pkg.name %>.css': ['build/<%= pkg.name %>.css']},
+				{'dist/<%= pkg.name %>.js': ['build/<%= pkg.name %>.js']},
+				{'dist/<%= pkg.name %>.min.js': ['build/<%= pkg.name %>.min.js']}
+			]
+		}
+	}
+
+});
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.registerTask('default', ['concat','uglify']);
+grunt.registerTask('dist', ['default','copy']);
+
+};
---
+++ dist/knockoutjs-sparkline.js
@@ -0,0 +1,72 @@
+define(["jquery","knockout"], function($,ko) {
+console.log('include');
+
+/**
+ * Sparkline custom binding
+ */
+ko.bindingHandlers.sparkLine = {
+
+	/**
+     * ko binding init
+     */
+    init: function(element, valueAccessor, allBindingsAccessor, deprecated, bindingContext) {
+    	console.log('--INIT:'+element.id);
+
+		var observable = valueAccessor() || { };
+
+		/**
+		 * Destroy sparkline
+		 *
+		 * Handle disposal (if KO removes by the template binding)
+		 */
+		ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+			console.log('Destroy sparkline');
+		});
+
+
+       	/**
+		 * Initalise sparkline.
+		 */
+		var now = new Date().getTime();
+        $(element).sparkline(data, {
+            type: 'line',
+            height: '22px',
+            width: '80px',
+            fillColor: '#72e572',
+            lineColor: '#000000',
+            spotColor: '#f08000',
+            minSpotColor: undefined,
+            maxSpotColor: undefined,
+            highlightSpotColor: undefined,
+            highlightLineColor: undefined,
+            chartRangeMinX: now - (60*60*4*1000),
+            chartRangeMaxX: now + 60*1000, // Add 60 seconds to create space for mouseover
+            chartRangeMin: -20,
+            chartRangeMax: 100,
+            normalRangeMin: 5,
+            normalRangeMax: 60,
+            normalRangeColor: '#e55e5b',
+            drawNormalOnTop: true,
+            tooltipFormatter: function (sparkline, options, fields) {
+                //return epochToLocal(fields.x, dateformat) + ': ' + fields.y + '°'; // TODO: Get units from temperature record.
+                return fields.x + ': ' + fields.y + '°'; // TODO: Get units from temperature record.
+            }
+        });
+
+		//return { controlsDescendantBindings: true };
+
+    },
+
+    /**
+     * ko binding update
+     */
+    update: function(element, valueAccessor, allBindingsAccessor, deprecated, bindingContext) {
+        console.log('--UPDATE:'+element.id);
+        var observable = valueAccessor() || { };
+        var peeked = ko.unwrap(observable.peek());
+        console.log(JSON.stringify(peeked));
+
+    }
+
+};
+});
