188acefbee3a026f849ca7e3806f2aefa57dc912
mryellow <mr-yellow@mr-yellow.com>
2014-11-15 09:06:41 +1100

    Looks right...

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ knockoutjs-sparkline
 ====================
 
 KnockoutJS custom binding for Omnipotent jQuery Sparkline
+
+### Dependancies
+
+* [http://jquery.com/](http://jquery.com/)
+* [http://knockoutjs.com/](http://knockoutjs.com/)
+* [http://omnipotent.net/jquery.sparkline/](http://omnipotent.net/jquery.sparkline/)
+* [http://momentjs.com/](http://momentjs.com/)
+
+### Extended options
+
+#### timeWindowMin
+Number of miliseconds to minus from `now` JS epoch.
+e.g. 4 hour window = `(60*60*4*1000)`
+
+#### timeWindowMax:
+Number of miliseconds to add to `now` JS epoch.
+e.g. Leaving a little space on the right = `(60*1000)`
+
+#### dateFormat
+For Moment localisation of dates.
+
+* [Moment docs](http://momentjs.com/docs/#/parsing/string-format/)
--- src/knockoutjs-sparkline.js
+++ src/knockoutjs-sparkline.js
@@ -3,62 +3,95 @@
  *
  * @author: Mr-Yellow <mr-yellow@mr-yellow.com>
  */
-console.log('include');
+define(['jquery','knockout','moment','sparkline'], function($,ko, moment) {
 
 /**
  * Sparkline custom binding
  */
 ko.bindingHandlers.sparkLine = {
 
-	/**
+    data: [],
+    defaults: {
+        type: 'line',
+        height: '22px',
+        width: '80px',
+        fillColor: '#72e572',
+        lineColor: '#000000',
+        spotColor: '#f08000',
+        minSpotColor: undefined,
+        maxSpotColor: undefined,
+        highlightSpotColor: undefined,
+        highlightLineColor: undefined,
+        chartRangeMin: -20,
+        chartRangeMax: 100,
+        normalRangeMin: 5,
+        normalRangeMax: 60,
+        normalRangeColor: '#e55e5b',
+        drawNormalOnTop: true,
+        tooltipFormatter: function (sparkline, options, fields) {
+            console.log([fields.x,fields.y]);
+            return ko.bindingHandlers.sparkLine.epochToLocal(fields.x) + ': ' + fields.y;
+        },
+        timeWindowMin: (60*60*4*1000),
+        timeWindowMax: (60*1000),
+        dateFormat: 'LLLL'
+    },
+    epochToLocal: function(epoch) {
+        if (epoch) {
+            // JS Epoch to localised format
+            return moment(epoch).format(ko.bindingHandlers.sparkLine.defaults.dateFormat);
+        }
+    },
+
+    /**
      * ko binding init
      */
     init: function(element, valueAccessor, allBindingsAccessor, deprecated, bindingContext) {
-    	console.log('--INIT:'+element.id);
+        console.log('--INIT:'+element.id);
 
-		var observable = valueAccessor() || { };
+        var observable = valueAccessor() || { };
+        var unwrapped = ko.unwrap(observable);
 
-		/**
-		 * Destroy sparkline
-		 *
-		 * Handle disposal (if KO removes by the template binding)
-		 */
-		ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-			console.log('Destroy sparkline');
-		});
+        ko.bindingHandlers.sparkLine.data = unwrapped;
+        
+        if (allBindingsAccessor().hasOwnProperty('sparkType')) ko.bindingHandlers.sparkLine.defaults.type = allBindingsAccessor().sparkType;
 
+        if (allBindingsAccessor().hasOwnProperty('sparkHeight')) ko.bindingHandlers.sparkLine.defaults.height = allBindingsAccessor().sparkHeight;
+        if (allBindingsAccessor().hasOwnProperty('sparkWidth')) ko.bindingHandlers.sparkLine.defaults.width = allBindingsAccessor().sparkWidth;
 
-       	/**
-		 * Initalise sparkline.
-		 */
-		var now = new Date().getTime();
-        $(element).sparkline(data, {
-            type: 'line',
-            height: '22px',
-            width: '80px',
-            fillColor: '#72e572',
-            lineColor: '#000000',
-            spotColor: '#f08000',
-            minSpotColor: undefined,
-            maxSpotColor: undefined,
-            highlightSpotColor: undefined,
-            highlightLineColor: undefined,
-            chartRangeMinX: now - (60*60*4*1000),
-            chartRangeMaxX: now + 60*1000, // Add 60 seconds to create space for mouseover
-            chartRangeMin: -20,
-            chartRangeMax: 100,
-            normalRangeMin: 5,
-            normalRangeMax: 60,
-            normalRangeColor: '#e55e5b',
-            drawNormalOnTop: true,
-            tooltipFormatter: function (sparkline, options, fields) {
-                //return epochToLocal(fields.x, dateformat) + ': ' + fields.y + '°'; // TODO: Get units from temperature record.
-                return fields.x + ': ' + fields.y + '°'; // TODO: Get units from temperature record.
-            }
-        });
+        if (allBindingsAccessor().hasOwnProperty('sparkChartRangeMin')) ko.bindingHandlers.sparkLine.defaults.chartRangeMin = allBindingsAccessor().sparkChartRangeMin;
+        if (allBindingsAccessor().hasOwnProperty('sparkChartRangeMax')) ko.bindingHandlers.sparkLine.defaults.chartRangeMax = allBindingsAccessor().sparkChartRangeMax;
+        if (allBindingsAccessor().hasOwnProperty('sparkNormalRangeMin')) ko.bindingHandlers.sparkLine.defaults.normalRangeMin = allBindingsAccessor().sparkNormalRangeMin;
+        if (allBindingsAccessor().hasOwnProperty('sparkNormalRangeMax')) ko.bindingHandlers.sparkLine.defaults.normalRangeMax = allBindingsAccessor().sparkNormalRangeMax;
 
-		//return { controlsDescendantBindings: true };
+        if (allBindingsAccessor().hasOwnProperty('sparkFillColor')) ko.bindingHandlers.sparkLine.defaults.fillColor = allBindingsAccessor().sparkFillColor;
+        if (allBindingsAccessor().hasOwnProperty('sparkLineColor')) ko.bindingHandlers.sparkLine.defaults.lineColor = allBindingsAccessor().sparkLineColor;
+        if (allBindingsAccessor().hasOwnProperty('sparkSpotColor')) ko.bindingHandlers.sparkLine.defaults.spotColor = allBindingsAccessor().sparkSpotColor;
+        if (allBindingsAccessor().hasOwnProperty('sparkNormalRangeColor')) ko.bindingHandlers.sparkLine.defaults.normalRangeColor = allBindingsAccessor().sparkNormalRangeColor;
 
+        if (allBindingsAccessor().hasOwnProperty('sparkChartRangeMinX')) ko.bindingHandlers.sparkLine.defaults.chartRangeMinX = allBindingsAccessor().sparkChartRangeMinX;
+        if (allBindingsAccessor().hasOwnProperty('sparkChartRangeMaxX')) ko.bindingHandlers.sparkLine.defaults.chartRangeMaxX = allBindingsAccessor().sparkChartRangeMaxX;
+
+        if (allBindingsAccessor().hasOwnProperty('sparkTimeWindowMin')) ko.bindingHandlers.sparkLine.defaults.timeWindowMin = allBindingsAccessor().sparkTimeWindowMin;
+        if (allBindingsAccessor().hasOwnProperty('sparkTimeWindowMax')) ko.bindingHandlers.sparkLine.defaults.timeWindowMax = allBindingsAccessor().sparkTimeWindowMax;
+
+        if (allBindingsAccessor().hasOwnProperty('sparkDateformat')) ko.bindingHandlers.sparkLine.defaults.dateFormat = allBindingsAccessor().sparkDateformat;
+
+        if (allBindingsAccessor().hasOwnProperty('sparkTooltipFormatter')) ko.bindingHandlers.sparkLine.defaults.tooltipFormatter = allBindingsAccessor().sparkTooltipFormatter;
+
+        /**
+         * Recalculate window.
+         */
+        var now = new Date().getTime();
+        ko.bindingHandlers.sparkLine.defaults.chartRangeMinX = now - ko.bindingHandlers.sparkLine.defaults.timeWindowMin;
+        ko.bindingHandlers.sparkLine.defaults.chartRangeMaxX = now - ko.bindingHandlers.sparkLine.defaults.timeWindowMan;
+
+        /**
+         * Initalise sparkline.
+         */
+        $(element).sparkline(ko.bindingHandlers.sparkLine.data, ko.bindingHandlers.sparkLine.defaults);
+
+        //return { controlsDescendantBindings: true };
     },
 
     /**
@@ -66,10 +99,22 @@ ko.bindingHandlers.sparkLine = {
      */
     update: function(element, valueAccessor, allBindingsAccessor, deprecated, bindingContext) {
         console.log('--UPDATE:'+element.id);
+
         var observable = valueAccessor() || { };
-        var peeked = ko.unwrap(observable.peek());
-        console.log(JSON.stringify(peeked));
+        var unwrapped = ko.unwrap(observable);
+
+        ko.bindingHandlers.sparkLine.data = unwrapped;
+
+        /**
+         * Recalculate window.
+         */
+        var now = new Date().getTime();
+        ko.bindingHandlers.sparkLine.defaults.chartRangeMinX = now - ko.bindingHandlers.sparkLine.defaults.timeWindowMin;
+        ko.bindingHandlers.sparkLine.defaults.chartRangeMaxX = now - ko.bindingHandlers.sparkLine.defaults.timeWindowMan;
+
+        $(element).sparkline(ko.bindingHandlers.sparkLine.data, ko.bindingHandlers.sparkLine.defaults);
 
     }
 
 };
+});
